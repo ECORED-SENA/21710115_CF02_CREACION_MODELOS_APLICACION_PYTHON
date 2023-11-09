086ffcf06e06cb8ba98bc94a87d2b0841e0f8262
elrilacho <camilobr17@gmail.com>
2023-11-09 07:05:15 -0500

    PDF_listo

--- src/config/global.js
+++ src/config/global.js
@@ -112,7 +112,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CF02_21710115_DU.pdf',
       },
       {
         icono: 'fas fa-download',
